09fdc4e728ddb5191d11ae8d7cb4c8ba05f12dce
qugebert <44033795+qugebert@users.noreply.github.com>
2024-05-30 12:30:50 +0200

    PowerPolesMaterial: Add answer "Connected to a transformer building"

--- app/src/main/java/de/westnordost/streetcomplete/quests/powerpoles_material/AddPowerPolesMaterialForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/powerpoles_material/AddPowerPolesMaterialForm.kt
@@ -1,13 +1,31 @@
 package de.westnordost.streetcomplete.quests.powerpoles_material
 
+import androidx.appcompat.app.AlertDialog
+import de.westnordost.streetcomplete.R
 import de.westnordost.streetcomplete.quests.AImageListQuestForm
+import de.westnordost.streetcomplete.quests.AnswerItem
 
 class AddPowerPolesMaterialForm : AImageListQuestForm<PowerPolesMaterial, PowerPolesMaterial>() {
 
     override val items = PowerPolesMaterial.values().map { it.asItem() }
     override val itemsPerRow = 3
 
+    override val otherAnswers = listOf(
+        AnswerItem(R.string.quest_powerPolesMaterial_terminal) { confirmIsTerminal() }
+    )
+
     override fun onClickOk(selectedItems: List<PowerPolesMaterial>) {
         applyAnswer(selectedItems.single())
+    }
+
+    private fun confirmIsTerminal() {
+        AlertDialog.Builder(requireContext())
+            .setTitle(R.string.quest_powerPolesMaterial_terminal_dialog_title)
+            .setMessage(R.string.quest_powerPolesMaterial_terminal_dialog_description)
+            .setPositiveButton(R.string.quest_generic_hasFeature_yes) { _, _ -> applyAnswer(
+                PowerPolesMaterial.TERMINAL
+            ) }
+            .setNegativeButton(android.R.string.cancel, null)
+            .show()
     }
 }
